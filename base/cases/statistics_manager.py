ed18c75da770fdc681061ae442c2e9e4eb1989b6
Baohua Yang <yangbaohua@gmail.com>
2013-11-03 17:14:21 +0800

    Add get_flow_stats function.

--- base/cases/statistics_manager.py
+++ base/cases/statistics_manager.py
@@ -15,3 +15,18 @@ class StatisticsManager(TestModule):
     """
     def __init__(self,restSubContext='/controller/nb/v2/statistics',user=DEFAULT_USER, password=DEFAULT_PWD,container=DEFAULT_CONTAINER,contentType='json',prefix=DEFAULT_PREFIX):
        super(self.__class__,self).__init__(restSubContext,user,password,container,contentType,prefix)
+
+    def get_flow_stats(self,suffix='flow'):
+        """
+        The name is suggested to match the NB API.
+        Show the flows
+        >>> StatisticsManager().get_flow_stats()
+        True
+        True
+        True
+        """
+        r=super(self.__class__,self).read(suffix)
+        if r:
+            print {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:01'} in r
+            print {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:02'} in r
+            print {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:03'} in r
