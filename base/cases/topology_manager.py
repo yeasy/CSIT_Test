b6bf799f542c98fa583a9356cf3038de30a17194
Baohua Yang <yangbaohua@gmail.com>
2013-11-03 16:41:28 +0800

    Add get_topology function test.

--- base/cases/topology_manager.py
+++ base/cases/topology_manager.py
@@ -15,15 +15,21 @@ class TopologyManager(TestModule):
     def __init__(self,restSubContext='/controller/nb/v2/topology',user=DEFAULT_USER, password=DEFAULT_PWD,container=DEFAULT_CONTAINER,contentType='json',prefix=DEFAULT_PREFIX):
        super(self.__class__,self).__init__(restSubContext,user,password,container,contentType,prefix)
 
-    def get_topology(self,suffix='/default'):
+    def get_topology(self,suffix=''):
         """
         The name is suggested to match the NB API.
         Show the topology
         >>> TopologyManager().get_topology()
+        8
+        True
         True
         True
         True
         """
         r=super(self.__class__,self).read(suffix)
         if r:
-            print r
+            print len(r)
+            print {u'tailNodeConnector': {u'node': {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:01'}, u'type': u'OF', u'id': u'2'}, u'headNodeConnector': {u'node': {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:03'}, u'type': u'OF', u'id': u'3'}} in r
+            print {u'tailNodeConnector': {u'node': {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:01'}, u'type': u'OF', u'id': u'1'}, u'headNodeConnector': {u'node': {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:02'}, u'type': u'OF', u'id': u'3'}} in r
+            print {u'tailNodeConnector': {u'node': {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:02'}, u'type': u'OF', u'id': u'3'}, u'headNodeConnector': {u'node': {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:01'}, u'type': u'OF', u'id': u'1'}} in r
+            print {u'tailNodeConnector': {u'node': {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:03'}, u'type': u'OF', u'id': u'3'}, u'headNodeConnector': {u'node': {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:01'}, u'type': u'OF', u'id': u'2'}} in r
