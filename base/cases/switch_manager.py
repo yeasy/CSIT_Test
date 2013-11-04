4bc04eb06fe1d3cd77515aca03e0648276a61015
Baohua Yang <yangbaohua@gmail.com>
2013-11-04 12:58:23 +0800

    Now test_node_property_operations can accept node_type and node_id as parameters.

--- base/cases/switch_manager.py
+++ base/cases/switch_manager.py
@@ -58,27 +58,32 @@ class SwitchManager(TestModule):
                    {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:02'} in r and \
                    {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:03'} in r)
 
-    def test_node_property_operations(self,suffix,property,value):
+    def test_node_property_operations(self,node_type,node_id,property,value):
         """
         Test the add,remove,show actions on node properties.
         Remove a property from given node.
 
-        >>> SwitchManager().test_node_property_operations('node/OF/00:00:00:00:00:00:00:02/property','description','Switch2')
+        >>> SwitchManager().test_node_property_operations('OF','00:00:00:00:00:00:00:01','description','Switch1')
+        True
+        True
+        True
+        >>> SwitchManager().test_node_property_operations('OF','00:00:00:00:00:00:00:02','description','Switch2')
         True
         True
         True
         """
+        suffix='node/'+node_type+'/'+node_id+'/property'
         #current node properties should not include description
         r=self.get_nodes()
-        index = r.index({u'type': u'OF', u'id': u'00:00:00:00:00:00:00:02'})
+        index = r.index({u'type': node_type, u'id': node_id})
         print r[index+1][property] == {u'value': u'None'}
         #After adding, current node properties should include description
         self.add_property_to_node(suffix+'/'+property+'/'+value)
         r=self.get_nodes()
-        index = r.index({u'type': u'OF', u'id': u'00:00:00:00:00:00:00:02'})
+        index = r.index({u'type': node_type, u'id': node_id})
         print r[index+1][property] ==  {u'value': value}
         #After removing, current node properties should not include description
         self.remove_property_from_node(suffix+'/'+property)
         r=self.get_nodes()
-        index = r.index({u'type': u'OF', u'id': u'00:00:00:00:00:00:00:02'})
+        index = r.index({u'type': node_type, u'id': node_id})
         print r[index+1][property] ==  {u'value': u'None'}
