79487fde678ff94917530d2af6b4045db68e7835
Baohua Yang <yangbaohua@gmail.com>
2013-11-04 11:38:08 +0800

    Add test on list_nodes and add/remove node properties.

--- base/cases/switch_manager.py
+++ base/cases/switch_manager.py
@@ -14,27 +14,71 @@ class SwitchManager(TestModule):
     def __init__(self,restSubContext='/controller/nb/v2/switchmanager',user=DEFAULT_USER, password=DEFAULT_PWD,container=DEFAULT_CONTAINER,contentType='json',prefix=DEFAULT_PREFIX):
        super(self.__class__,self).__init__(restSubContext,user,password,container,contentType,prefix)
 
-    def get_nodes(self,suffix='nodes'):
+    def get_nodes(self):
         """
         The name is suggested to match the NB API.
         list all nodes and their properties
-        >>> SwitchManager().get_nodes()
-        True
-        True
-        True
+        """
+        suffix='nodes'
+        r=super(self.__class__,self).read(suffix)
+        if r:
+            return r
+
+    def get_node(self,suffix):
+        """
+        The name is suggested to match the NB API.
+        list nodeconnector and properties of a node.
         """
         r=super(self.__class__,self).read(suffix)
         if r:
-            print {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:01'} in r
-            print {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:02'} in r
-            print {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:03'} in r
+            print r
 
-    def add_property_to_node(self,suffix='node/OF/00:00:00:00:00:00:00:02/property/description/Switch2'):
+    def add_property_to_node(self,suffix):
         """
-        Add a property to some node.
-
-        >>> SwitchManager().add_property_to_node()
-        201
+        Add a property to given node.
         """
         r=super(self.__class__,self).update(suffix)
-        print r
+
+    def remove_property_from_node(self,suffix):
+        """
+        Remove a property from given node.
+        """
+        r=super(self.__class__,self).delete(suffix)
+
+    def test_list_nodes(self):
+        """
+        The name is suggested to match the NB API.
+        list all nodes and their properties
+        >>> SwitchManager().test_list_nodes()
+        True
+        """
+        r=self.get_nodes()
+        if r:
+            print ({u'type': u'OF', u'id': u'00:00:00:00:00:00:00:01'} in r and \
+                   {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:02'} in r and \
+                   {u'type': u'OF', u'id': u'00:00:00:00:00:00:00:03'} in r)
+
+    def test_node_property_operations(self,suffix):
+        """
+        Test the add,remove,show actions on node properties.
+        Remove a property from given node.
+
+        >>> SwitchManager().test_node_property_operations('node/OF/00:00:00:00:00:00:00:02')
+        True
+        True
+        True
+        """
+        #current node properties should not include description
+        r=self.get_nodes()
+        index = r.index({u'type': u'OF', u'id': u'00:00:00:00:00:00:00:02'})
+        print r[index+1][u'description'] == {u'value': u'None'}
+        #After adding, current node properties should include description
+        self.add_property_to_node('node/OF/00:00:00:00:00:00:00:02/property/description/Switch2')
+        r=self.get_nodes()
+        index = r.index({u'type': u'OF', u'id': u'00:00:00:00:00:00:00:02'})
+        print r[index+1][u'description'] ==  {u'value': u'Switch2'}
+        #After removing, current node properties should not include description
+        self.remove_property_from_node('node/OF/00:00:00:00:00:00:00:02/property/description')
+        r=self.get_nodes()
+        index = r.index({u'type': u'OF', u'id': u'00:00:00:00:00:00:00:02'})
+        print r[index+1][u'description'] ==  {u'value': u'None'}
