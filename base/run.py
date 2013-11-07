5ed7a1d01481752930a27cfedd50ead5bb1d399c
Baohua Yang <yangbaohua@gmail.com>
2013-11-07 14:56:29 +0800

    Refine the module loading process.

--- base/run.py
+++ base/run.py
@@ -2,7 +2,7 @@
 """
 CSIT test tools.
 Authors: Baohua Yang@IBM, Denghui Huang@IBM
-Updated: 2013-11-01
+Updated: 2013-11-07
 
 Usage: Before running the test tool, should
  1. Start 2-layer tree topology network. e.g., in Mininet, run  'sudo mn --controller=remote,ip=127.0.0.1 --mac --topo tree,2'.
@@ -11,7 +11,6 @@ Usage: Before running the test tool, should
 """
 import doctest
 import os
-
 from restlib import *
 
 
@@ -19,8 +18,8 @@ def test_case(module_name):
     '''
     Run single test on given module.
     '''
-    print "#Test case: " + module_name.split('/')[1][:-3].replace('_',' ')
-    cmd = 'python -m doctest '+module_name
+    print "#Test case: " + module_name.replace('_', ' ')
+    cmd = 'python -m doctest ' + module_name + '.py'
     os.system(cmd)
 
 def run(modules=None):
@@ -29,19 +28,19 @@ def run(modules=None):
     If no parameter is given, then will scan the case directory,
      and try to run all cases.
     '''
-    if modules:
-        for name in modules:
-            test_case(CASES_DIR+'/'+name+'.py')
-    else:
-        for root,dirs,files in os.walk(CASES_DIR,topdown=True):
-            for name in files:
-                test_case(root+'/'+name)
+    backup_dir = os.getcwd()
+    if not modules:
+        modules = [e[:-3] for e in os.listdir(CASES_DIR) if e.endswith('.py')]
+    os.chdir(backup_dir + '/' + CASES_DIR)
+    for name in modules:
+        test_case(name)
+    os.chdir(backup_dir)
+
 
 if __name__ == '__main__':
     doctest.testmod()
     test_modules = ['switch_manager', 'topology_manager', 'forwarding_rule_manager', 'statistics_manager',
                     'host_tracker', 'arp_handler', 'forwarding_manager', 'container_manager']
-    #test_modules = ['forwarding_rule_manager']
-    #test_modules = ['switch_manager']
+    #test_modules = ['topology_manager']
     run(test_modules)
     #run()
